30a0f8d51d274df32f2c6e3607f84fbca6782e12
Mark Skelton <mdskelton99@gmail.com>
2023-05-21 12:01:26 -0500

    Fix TSC

--- src/preload.ts
+++ src/preload.ts
@@ -9,8 +9,8 @@ function click(element: Node | null) {
 const readyOpts = { stopOnDomReady: false }
 
 // Use same shortcut for submit no matter the screen size
-ready("#prompt-textarea", readyOpts).then((element: HTMLElement) => {
-  element.addEventListener("keydown", (event) => {
+ready("#prompt-textarea", readyOpts).then((element) => {
+  element?.addEventListener("keydown", (event) => {
     if (event.key === "Enter" && !event.metaKey && !event.shiftKey) {
       event.preventDefault()
       click(element.nextSibling)
