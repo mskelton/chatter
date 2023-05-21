78b9f0105515d9b720832531a56f502fe043733d
Mark Skelton <mdskelton99@gmail.com>
2023-05-20 21:02:11 -0500

    Icons!

--- forge.config.js
+++ forge.config.js
@@ -1,14 +1,39 @@
+/**
+ * Get's the x or y location for the contents. This adjusts based on the size
+ * of the placeholders.
+ */
+const loc = (pos) => pos + 130 / 2
+
+/**
+ * Offset the y-location by a touch to make it visually appear centered due
+ * to the text underneath.
+ */
+const y = (pos) => loc(pos) - 10
+
+/** @type {import("@electron-forge/shared-types").ForgeConfig} */
 module.exports = {
   makers: [
     {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
       config: {
         background: "./assets/dmg-background.png",
+        contents: (opts) => [
+          { path: "/Applications", type: "link", x: loc(427), y: y(85) },
+          { path: opts.appPath, type: "file", x: loc(42), y: y(85) },
+        ],
         format: "ULFO",
+        icon: "./assets/icons/icon.icns",
       },
       name: "@electron-forge/maker-dmg",
     },
   ],
-  packagerConfig: {},
+  packagerConfig: {
+    appBundleId: "dev.mskelton.chatter",
+    icon: "./assets/icons/icon",
+  },
   plugins: [
     {
       config: {
