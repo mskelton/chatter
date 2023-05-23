bebb47116f8207fb0549e4b9457b4dc350fe2e27
Mark Skelton <mdskelton99@gmail.com>
2023-05-22 19:01:02 -0500

    Testing

--- forge.config.js
+++ forge.config.js
@@ -34,15 +34,15 @@ module.exports = {
   packagerConfig: {
     appBundleId: 'dev.mskelton.chatter',
     icon: './assets/icons/icon',
-    // osxNotarize: {
-    //   appleId: process.env.APPLE_ID,
-    //   appleIdPassword: process.env.APPLE_PASSWORD,
-    //   teamId: process.env.APPLE_TEAM_ID,
-    //   tool: 'notarytool',
-    // },
-    // osxSign: {
-    //   platform: 'darwin',
-    // },
+    osxNotarize: {
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+      tool: 'notarytool',
+    },
+    osxSign: {
+      identity: process.env.APPLE_IDENTITY,
+    },
   },
   plugins: [
     {
