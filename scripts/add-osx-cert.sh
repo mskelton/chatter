c1ecaee346e30888c507dcad178fe4d5d6e38e21
Mark Skelton <mdskelton99@gmail.com>
2023-05-25 07:58:53 -0500

    Finish script

--- scripts/add-osx-cert.sh
+++ scripts/add-osx-cert.sh
@@ -6,7 +6,7 @@ MACOS_CERT_P12_FILE=certificate.p12
 # Recreate the certificate from the secure environment variable
 echo $MACOS_CERT_P12 | base64 --decode >$MACOS_CERT_P12_FILE
 
-#create a keychain
+# Create a keychain
 security create-keychain -p actions $KEY_CHAIN
 
 # Make the keychain the default so identities are found
@@ -25,14 +25,10 @@ security import ./assets/certs/AppleWWDRCAG4.cer -k $KEY_CHAIN -T /usr/bin/codes
 security import ./assets/certs/DeveloperIDG2CA.cer -k $KEY_CHAIN -T /usr/bin/codesign
 
 # Developer cert
-security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P $MACOS_CERT_PASSWORD -T /usr/bin/codesign
+security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P "$MACOS_CERT_PASSWORD" -T /usr/bin/codesign
 
 # Set the partition list (sort of like an access control list)
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
 # remove certs
 rm -fr *.p12
-
-# Echo the identity, just so that we know it worked.
-# This won't display anything secret.
-security find-identity -v -p codesigning $KEY_CHAIN
