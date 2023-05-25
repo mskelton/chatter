c68c42ae5d79b7e80279ba74b30f672a6bf12452
Mark Skelton <mdskelton99@gmail.com>
2023-05-25 08:16:20 -0500

    No notarize

--- forge.config.ts
+++ forge.config.ts
@@ -54,12 +54,12 @@ const config: ForgeConfig = {
     appBundleId: 'dev.mskelton.chatter',
     appCategoryType: 'public.app-category.developer-tools',
     icon: './assets/icons/icon',
-    osxNotarize: {
-      appleId: process.env.APPLE_ID ?? '',
-      appleIdPassword: process.env.APPLE_PASSWORD ?? '',
-      teamId: process.env.APPLE_TEAM_ID ?? '',
-      tool: 'notarytool',
-    },
+    // osxNotarize: {
+    //   appleId: process.env.APPLE_ID ?? '',
+    //   appleIdPassword: process.env.APPLE_PASSWORD ?? '',
+    //   teamId: process.env.APPLE_TEAM_ID ?? '',
+    //   tool: 'notarytool',
+    // },
     osxSign: {},
   },
   plugins: [
